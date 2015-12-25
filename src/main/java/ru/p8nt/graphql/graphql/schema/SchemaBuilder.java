cf7c14bc38b8f413a00df030f8ddbc81f779c2ae
Vyacheslav Slinko <vyacheslav.slinko@gmail.com>
2015-12-25 15:39:22 +0300

    Collect all type automatically

--- src/main/java/ru/p8nt/graphql/graphql/schema/SchemaBuilder.java
+++ src/main/java/ru/p8nt/graphql/graphql/schema/SchemaBuilder.java
@@ -4,34 +4,25 @@ import graphql.schema.GraphQLObjectType;
 import graphql.schema.GraphQLSchema;
 import graphql.schema.GraphQLType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Map;
 
 @Configuration
 public class SchemaBuilder {
     @Autowired
-    @Qualifier("userType")
-    private GraphQLObjectType userType;
-
-    @Autowired
-    @Qualifier("sessionType")
-    private GraphQLObjectType sessionType;
-
-    @Autowired
-    @Qualifier("queryType")
-    private GraphQLObjectType queryType;
+    private ApplicationContext applicationContext;
 
     @Bean
     public GraphQLSchema build() {
+        Map<String, GraphQLObjectType> types = applicationContext.getBeansOfType(GraphQLObjectType.class);
+        GraphQLObjectType queryType = types.get("queryType");
+
         return GraphQLSchema.newSchema()
                 .query(queryType)
-                .build(new HashSet<GraphQLType>(Arrays.asList(
-                        userType,
-                        sessionType
-                )));
+                .build(new HashSet<GraphQLType>(types.values()));
     }
 }
